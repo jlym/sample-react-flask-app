3f3d2d9964c3f5fb3725bd2720de6db785348b9a
Jeffrey Lym <jeffreyalym@gmail.com>
2021-07-03 13:37:47 -0700

    Add react router and home and dashboard pages

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,10 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
+import {HomePage} from "./pages/index";
+import {DashboardPage} from "./pages/dashboard";
 
 ReactDOM.render(
   <React.StrictMode>
-    
+
+    {
+      // Use React-Router to map routes to page components.
+      // https://reactrouter.com/web/guides/quick-start
+    }
+    <BrowserRouter>
+      <Switch>
+        <Route path="/dashboard">
+          <DashboardPage />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
